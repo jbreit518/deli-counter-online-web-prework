507fc5cb99da91a5bd33d7b21dc195de3e1ec355
Justine Berretta <justberretta@gmail.com>
2019-05-22 23:25:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(deli)
   if deli == []
     puts "The line is currently empty."
   else 
-    puts "The line is currently:#{deli}."
+    puts "The line is currently:#{deli.each}."
   end   
 end 
 
