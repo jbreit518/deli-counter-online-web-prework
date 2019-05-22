9b779e2c2370c72800085c04c7ebfdf12c92e7cf
Justine Berretta <justberretta@gmail.com>
2019-05-22 23:38:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,9 @@ def line(deli)
   if deli == []
     puts "The line is currently empty."
   else 
-    puts "The line is currently:
-    #{deli.each_with_index(1) { name, i }."
+    deli.each_with_index do |object, index|
+    indexplusone = index + 1 
+    puts "The line is currently: #{indexplusone}. #{object}"
   end   
 end 
 
